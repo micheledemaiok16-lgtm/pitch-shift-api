fd3cfa9800427614f762608cd1db47186cbbd0b9
Michele De Maio <micheledemaiok.16@gmail.com>
2026-03-18 13:46:38 +0100

    Primo commit

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def pitch():
     return send_file(out, mimetype='audio/mp4')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
